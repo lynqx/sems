8252dd12e5039a88cbedb4c811a1e7cd02769773
Akinjiola Toni [Toniton] <akinjiolatoni@gmail.com>
2017-01-16 14:10:49 +0100

    updates on sms thingy

---
+++ database/migrations/2017_01_16_095930_create_sms_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSmsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sms', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('sender');
+            $table->string('recipient');
+            $table->string('message');
+            $table->string('response');
+            $table->integer('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sms');
+    }
+}
