47a08c00eaaf77ebc46e59fdb35e02f41d51f9dc
Doubleakins <aakinjiola@gmail.com>
2017-01-24 06:34:22 +0100

    payment_migrations

---
+++ database/migrations/2017_01_24_052909_payments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class PaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('student_id');
+            $table->integer('amount');
+            $table->integer('session_id');
+            $table->integer('term_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+
+    }
+}
